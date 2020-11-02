7f8fad556dd41fde6a646fb879a37c36904dc026
Damian Duszyński <dd.duszynski@gmail.com>
2020-11-02 07:59:47 +0100

    working on boardCard UI

--- src/components/BoardCard/BoardCard.js
+++ src/components/BoardCard/BoardCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import clsx from 'clsx';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -9,7 +9,6 @@ import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -18,8 +17,10 @@ const im = "https://images.pexels.com/photos/3046632/pexels-photo-3046632.jpeg?a
 const useStyles = makeStyles((theme) => ({
    root: {
       maxWidth: '300px',
-      marginRight: '10px',
-      marginBottom: '10px',
+      marginRight: '15px',
+      marginBottom: '15px',
+      backgroundColor: '#444',
+      color: '#fff'
    },
    media: {
       height: 0,
@@ -29,9 +30,18 @@ const useStyles = makeStyles((theme) => ({
    avatar: {
       backgroundColor: theme.palette.primary.main,
    },
+   cardHeader: {
+      color: '#eee',
+   },
+   whiteIcon: {
+      color: '#eee',
+   },
+   redIcon: {
+      color: '#e11',
+   },
 }));
 
-export default function BoardCard({ title, starred, image, description }) {
+export default function BoardCard({ title, starred, image, description, link }) {
    const classes = useStyles();
    const [isStarred, setIsStarred] = useState(starred);
 
@@ -40,32 +50,37 @@ export default function BoardCard({ title, starred, image, description }) {
    };
 
    return (
+
       <Card className={classes.root}>
          <CardHeader
             avatar={
                <Avatar aria-label="recipe" className={classes.avatar}>
-                  R
+                  D
                </Avatar>
             }
             action={
                <IconButton aria-label="settings">
-                  <MoreVertIcon />
+                  <MoreVertIcon className={classes.whiteIcon}/>
                </IconButton>
             }
             title={title}
             subheader="November 01, 2020"
+            subheaderTypographyProps={{ 'color': 'white' }}
+            className={classes.cardHeader}
          />
+         <Link to={link ? link : "/"}>
+            <CardMedia
+               className={classes.media}
+               image={image}
+               title="Paella dish"
+            />
 
-         <CardMedia
-            className={classes.media}
-            image={image}
-            title="Paella dish"
-         />
+         </Link>
 
          <CardContent>
             <Typography
                variant="body2"
-               color="textSecondary"
+               // color="textSecondary"
                component="p"
             >
                {description}
@@ -76,13 +91,13 @@ export default function BoardCard({ title, starred, image, description }) {
             <IconButton
                aria-label="add to favorites"
                onClick={handleIsStarred}
-               >
-               <FavoriteIcon  
-                  color={isStarred ? 'error' : 'inherit' }
+            >
+               <FavoriteIcon
+                  className={isStarred ? classes.redIcon : classes.whiteIcon}
                />
             </IconButton>
             <IconButton aria-label="share">
-               <ShareIcon />
+               <ShareIcon className={classes.whiteIcon} />
             </IconButton>
          </CardActions>
       </Card>
--- src/pages/Templates/Templates.js
+++ src/pages/Templates/Templates.js
@@ -14,12 +14,19 @@ const useStyles = makeStyles((theme) => ({
       paddingRight: 24,
    },
 }))
+
 const Templates = () => {
    const classes = useStyles();
    return (
       <Main>
-         <Typography variant="h6" component="h1">
+         <Typography variant="h5" component="h1">
             Templates
+         </Typography>
+         <Typography variant="body1">
+            Get going faster with one of the ready-to-use predefined templates.
+         </Typography>
+         <Typography variant="h6" component="h1">
+            Business Templates
          </Typography>
          <Grid
             container
@@ -29,34 +36,178 @@ const Templates = () => {
             className={classes.root}
          >
             <BoardCard
-               title="Kanban basic board"
-               description="This impressive paella is a perfect party dish and a fun meal to cook together with your
-               guests. Add 1 cup of frozen peas along with the mussels, if you like."
-               image={`https://images.pexels.com/photos/4870969/pexels-photo-4870969.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`}
+               title="Business Templates 1"
+               description="Use this simple Kanban template to keep the engineering team on the same page and moving through work fluidly."
+               image={`https://images.pexels.com/photos/572056/pexels-photo-572056.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+               link="/board2"
             />
             <BoardCard
-               title="Lean Canvas"
-               description="This impressive paella is a perfect party dish and a fun meal to cook together with your
-               guests. Add 1 cup of frozen peas along with the mussels, if you like."
-               image={`https://images.pexels.com/photos/4870969/pexels-photo-4870969.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`}
+               title="Business Templates 2"
+               description="Lean Canvas is a 1-page business plan template that helps you deconstruct your idea into its key assumptions."
+               image={`https://images.pexels.com/photos/936137/pexels-photo-936137.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
             />
             <BoardCard
-               title="Weekly Planner"
-               description="This impressive paella is a perfect party dish and a fun meal to cook together with your
-               guests. Add 1 cup of frozen peas along with the mussels, if you like."
-               image={`https://images.pexels.com/photos/4870969/pexels-photo-4870969.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`}
+               title="Business Templates 3"
+               description="Plan every day of the week and after you're finished, just drag it to the done list!"
+               image={`https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+         </Grid>
+
+         <Typography variant="h6" component="h1">
+            Design Templates
+         </Typography>
+         <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-start"
+            className={classes.root}
+         >
+            <BoardCard
+               title="Design Templates 1"
+               description="The Eisenhower Matrix is a powerful method to organize your daily/weekly tasks. Read the explanation on how to use this method wisely in Trello."
+               image={`https://images.pexels.com/photos/5836/yellow-metal-design-decoration.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
             />
             <BoardCard
-               title="Agile Sprint"
-               description="This impressive paella is a perfect party dish and a fun meal to cook together with your
-               guests. Add 1 cup of frozen peas along with the mussels, if you like."
-               image={`https://images.pexels.com/photos/4870969/pexels-photo-4870969.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`}
+               title="Design Templates 2"
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/5933/color-paint-palette-wall-painting.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
             />
             <BoardCard
-               title="Eisenhower Matrix"
-               description="This impressive paella is a perfect party dish and a fun meal to cook together with your
-               guests. Add 1 cup of frozen peas along with the mussels, if you like."
-               image={`https://images.pexels.com/photos/4870969/pexels-photo-4870969.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`}
+               title="Design Templates 3"
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/5356427/pexels-photo-5356427.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+         </Grid>
+
+         <Typography variant="h6" component="h1">
+            Education Templates
+         </Typography>
+         <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-start"
+            className={classes.root}
+         >
+            <BoardCard
+               title="Education Templates 1"
+               description="The Eisenhower Matrix is a powerful method to organize your daily/weekly tasks. Read the explanation on how to use this method wisely in Trello."
+               image={`https://images.pexels.com/photos/4144923/pexels-photo-4144923.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+            <BoardCard
+               title="Education Templates 2"
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/2714073/pexels-photo-2714073.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+            <BoardCard
+               title="Education Templates 3"
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/5088008/pexels-photo-5088008.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+         </Grid>
+         <Typography variant="h6" component="h1">
+            {`Personal & Productivity`}
+         </Typography>
+         <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-start"
+            className={classes.root}
+         >
+            <BoardCard
+               title={`Personal & Productivity 1`}
+               description="The Eisenhower Matrix is a powerful method to organize your daily/weekly tasks. Read the explanation on how to use this method wisely in Trello."
+               image={`https://images.pexels.com/photos/724994/pexels-photo-724994.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+            <BoardCard
+               title={`Personal & Productivity 2`}
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/19677/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+            <BoardCard
+               title={`Personal & Productivity 3`}
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/669996/pexels-photo-669996.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+         </Grid>
+         <Typography variant="h6" component="h1">
+            Marketing
+         </Typography>
+         <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-start"
+            className={classes.root}
+         >
+            <BoardCard
+               title="Marketing 1"
+               description="The Eisenhower Matrix is a powerful method to organize your daily/weekly tasks. Read the explanation on how to use this method wisely in Trello."
+               image={`https://images.pexels.com/photos/669610/pexels-photo-669610.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+            <BoardCard
+               title="Marketing 2"
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/6231/marketing-color-colors-wheel.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+            <BoardCard
+               title="Marketing 3"
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/185576/pexels-photo-185576.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+         </Grid>
+         <Typography variant="h6" component="h1">
+            Project Management
+         </Typography>
+         <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-start"
+            className={classes.root}
+         >
+            <BoardCard
+               title="Project Management 1"
+               description="The Eisenhower Matrix is a powerful method to organize your daily/weekly tasks. Read the explanation on how to use this method wisely in Trello."
+               image={`https://images.pexels.com/photos/355952/pexels-photo-355952.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+            <BoardCard
+               title="Project Management 2"
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/268362/pexels-photo-268362.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+            <BoardCard
+               title="Project Management 3"
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/669615/pexels-photo-669615.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+         </Grid>
+         <Typography variant="h6" component="h1">
+            Remote Work
+         </Typography>
+         <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-start"
+            className={classes.root}
+         >
+            <BoardCard
+               title="Remote Work 1"
+               description="The Eisenhower Matrix is a powerful method to organize your daily/weekly tasks. Read the explanation on how to use this method wisely in Trello."
+               image={`https://images.pexels.com/photos/4099388/pexels-photo-4099388.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+            <BoardCard
+               title="Remote Work 2"
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/389818/pexels-photo-389818.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
+            />
+            <BoardCard
+               title="Remote Work 3"
+               description="Move fast without losing sight by adopting an agile workflow that gives your team perspective during any project management situation."
+               image={`https://images.pexels.com/photos/3568792/pexels-photo-3568792.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`}
             />
          </Grid>
       </Main>
